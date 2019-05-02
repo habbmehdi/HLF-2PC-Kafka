b03cbc0913d85501ff3127b04f76fbc8b56cdd1f
habbmehdi <habb.mehdi@gmail.com>
2019-05-02 22:16:07 +0100

    fourth commit

--- start_server_1.sh
+++ start_server_1.sh
@@ -10,8 +10,12 @@
 # start fabric containers
 docker-compose -f deployment/docker-compose-kafka.yml up -d
 
+sleep 10
+sleep 10
+
 # start cli container
 docker-compose -f deployment/docker-compose-cli.yml up -d
+
 
 docker exec -e "CORE_PEER_LOCALMSPID=Org1MSP" -e "CORE_PEER_MSPCONFIGPATH=/var/hyperledger/users/Admin@org1.example.com/msp" peer0.org1.example.com peer channel create -o orderer0.example.com:7050 -c mychannel -f /var/hyperledger/configs/channel.tx
 
